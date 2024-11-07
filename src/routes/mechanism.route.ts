57e01fe8bf3e9c1d7ba23d2bdb42f52d49321d9a
Maulana Zahiri <147925317+yolookings@users.noreply.github.com>
2024-11-07 19:03:14 +0700

    update mecanism .route

--- src/routes/mechanism.route.ts
+++ src/routes/mechanism.route.ts
@@ -1,11 +1,10 @@
-// src/routes/mechanism.route.ts
 import { Router } from "express";
 import MechanismController from "../controllers/mechanism.controller";
 import { Verification } from "../middleware/auth";
 
 const router = Router();
 
-router.post("/borrow/:id", MechanismController.borrowBook);
-router.post("/return/:id", MechanismController.returnBook);
+router.post("/borrow/:id", Verification, MechanismController.borrowBook);
+router.post("/return/:id", Verification, MechanismController.returnBook);
 
 export default router;
